eeda07b9b11cd65f0e3fbdc926005aefe8c221ad
Payton <pjavete@ucsc.edu>
2021-09-29 20:47:37 -0700

    Learning Circles

--- source/main.py
+++ source/main.py
@@ -1,6 +1,29 @@
+from tkinter import *
+import random
+
 def main():
-    color_set = ['Red', 'Green', 'Blue', 'Yellow', 'Brown', 'Orange', 'Black', 'White']
-    print(color_set[0])
+    root = Tk()
+    myCanvas = Canvas(root)
+    myCanvas.pack()
+
+    create_circle(100, 100, 20, myCanvas)
+    create_circle(50, 25, 10, myCanvas)
+
+    root.mainloop()
+
+def create_circle(x, y, r, color, canvasName): #center coordinates, radius, color
+    x0 = x - r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
+    return canvasName.create_oval(x0, y0, x1, y1, outline="#000", fill=color)
+
+# Generates a random code of size 'size' from set 'colors' with a duplicate switch
+def generate_code(duplicates, size, colors):
+    if duplicates:
+        return random.choice(colors, k=size)
+    else:
+        return random.sample(colors, size)
 
 if __name__ == "__main__":
     main()
